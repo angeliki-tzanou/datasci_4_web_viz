1a0749597cc1d37ed615dc4572ad2921e8bf2c33
angeliki-tzanou <angeliki.tzanou@stonybrook.edu>
2023-09-26 17:07:32 +0000

    added app code

--- shiny_python/app.py
+++ shiny_python/app.py
@@ -11,10 +11,10 @@ def load_data():
     return pd.read_csv(url)
 
 df = load_data()
-df_binge = df[(df['MeasureId'] == 'BINGE') & (df['Data_Value_Type'] == 'Age-adjusted prevalence')]
+df_copd = df[(df['MeasureId'] == 'COPD') & (df['Data_Value_Type'] == 'Age-adjusted prevalence')]
 
 # Available counties for selection
-counties = df_binge['LocationName'].unique()
+counties = df_copd['LocationName'].unique()
 
 app_ui = ui.page_fluid(
     ui.input_select("county", "Select County", {county: county for county in counties}),
@@ -28,21 +28,21 @@ def server(input, output, session):
     @render.text
     def avg_data_value():
         selected_county = input.county()
-        avg_value = df_binge[df_binge['LocationName'] == selected_county]['Data_Value'].mean()
-        return f"Average Binge Drinking Age-adjusted Prevalence for {selected_county}: {avg_value:.2f}%"
+        avg_value = df_copd[df_copd['LocationName'] == selected_county]['Data_Value'].mean()
+        return f"COPD indivials and Age-adjusted Prevalence for {selected_county}: {avg_value:.2f}%"
 
     @output
     @render.plot(alt="Binge Drinking Age-adjusted Prevalence Bar Chart")
     def bar_chart():
-        overall_avg = df_binge['Data_Value'].mean()
-        selected_county_avg = df_binge[df_binge['LocationName'] == input.county()]['Data_Value'].mean()
+        overall_avg = df_copd['Data_Value'].mean()
+        selected_county_avg = df_copd[df_copd['LocationName'] == input.county()]['Data_Value'].mean()
 
         fig, ax = plt.subplots(figsize=(10, 6))
         ax.bar(['Selected County', 'Overall Average'], [selected_county_avg, overall_avg], color=['lightcoral', 'dodgerblue'])
         
         ax.set_ylabel('Data Value (Age-adjusted prevalence) - Percent')
         ax.set_ylim(0, 30)
-        ax.set_title('Binge Drinking Age-adjusted Prevalence Comparison')
+        ax.set_title('COPD Age-adjusted Prevalence Comparison')
         
         return fig
 
